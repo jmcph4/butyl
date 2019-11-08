4b54038e37506b39a4146bf7ef3de18f2cea12eb
jmcph4 <jmcph4@users.noreply.github.com>
2019-11-08 12:31:31 +1000

    Add function to instantiate correct binary format
    This commit adds the `utils::get_file_as` function, which populates
    a `formats::File` enum with the specified binary format handle.

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,18 @@
+use crate::errors;
 use crate::formats;
+use crate::dos;
+use crate::coff;
 
 pub fn infer_format(data: &[u8]) -> formats::Format {
    unimplemented!(); 
+}
+
+pub fn get_file_as(data: &[u8], format: formats::Format) ->
+    Result<formats::File, errors::ButylError> {
+    Ok(match format {
+        DOS => formats::File::DOS(dos::DosFile::from_le_bytes(data)?),
+        COFF => formats::File::COFF(coff::CoffFile::from_le_bytes(data)?),
+        _ => formats::File::Unknown(())
+    })
 }
 
